d2430bd2c76966ea4cd7b61a24857f8e9e0c56c1
Dromader2137 <wojciechowski.ignacy@gmail.com>
2024-03-07 12:14:52 +0100

    curl rename

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@
 token=$(./login-curl.sh localhost:8000 root toor)
 tok=${token:1:-1}
 echo "$tok"
-./fill-db-curl.sh localhost:8000 hello welcome yo $tok
-./fill-db-curl.sh localhost:8000 hi welcome ipex $tok 
-./fill-db-curl.sh localhost:8000 hello response yo $tok
+./create-post.sh localhost:8000 hello welcome yo $tok
+./create-post.sh localhost:8000 hi welcome ipex $tok 
+./create-post.sh localhost:8000 hello response yo $tok

9b0662b00bf0b48c79a961cc3073143f96057296
Dromader2137 <wojciechowski.ignacy@proton.me>
2024-04-10 17:37:19 +0200

    scripts 2

--- scripts/test-bans.sh
+++ scripts/test-bans.sh
@@ -2,18 +2,18 @@
 
 ip=$1
 
-token_0=$(./login.sh $ip root toor)
+token_0=$(./scripts/login.sh $ip root toor)
 tok_0=${token_0:1:-1}
-token_1=$(./signup.sh $ip abc abc)
+token_1=$(./scripts/signup.sh $ip abc abc)
 tok_1=${token_1:1:-1}
-token_2=$(./signup.sh $ip def def)
+token_2=$(./scripts/signup.sh $ip def def)
 tok_2=${token_2:1:-1}
-./create-post.sh $ip abc a b $tok_1
-./create-post.sh $ip def d e $tok_2
-./ban.sh $ip 1 1000 "za bycie sprzedajna kurwa" $tok_0
+./scripts/create-post.sh $ip abc a b $tok_1
+./scripts/create-post.sh $ip def d e $tok_2
+./scripts/ban.sh $ip 1 1000 "za bycie sprzedajna kurwa" $tok_0
 echo
-./login.sh $ip abc abc
+./scripts/login.sh $ip abc abc
 echo
-./unban.sh $ip 1 $tok_0
-./login.sh $ip abc abc
+./scripts/unban.sh $ip 1 $tok_0
+./scripts/login.sh $ip abc abc
 echo
--- scripts/test-images.sh
+++ scripts/test-images.sh
@@ -2,40 +2,40 @@
 
 ip=$1
 
-token_0=$(./login.sh $ip root toor)
+token_0=$(./scripts/login.sh $ip root toor)
 tok_0=${token_0:1:-1}
-token_1=$(./signup.sh $ip dr 1234)
+token_1=$(./scripts/signup.sh $ip dr 1234)
 tok_1=${token_1:1:-1}
-./create-post.sh $ip hello welcome yo $tok_1
+./scripts/create-post.sh $ip hello welcome yo $tok_1
 echo
-./upload-image.sh $ip ./media/profile_pictures/default.png $tok_1
+./scripts/upload-image.sh $ip ./media/profile_pictures/default.png $tok_1
 echo
-./upload-image.sh $ip ./media/profile_pictures/default.png $tok_1
+./scripts/upload-image.sh $ip ./media/profile_pictures/default.png $tok_1
 echo
-./upload-image.sh $ip ./media/profile_pictures/default.png $tok_1
+./scripts/upload-image.sh $ip ./media/profile_pictures/default.png $tok_1
 echo
-./upload-image.sh $ip ./media/profile_pictures/default.png $tok_1
+./scripts/upload-image.sh $ip ./media/profile_pictures/default.png $tok_1
 echo
-./upload-image.sh $ip ./media/profile_pictures/default.png $tok_1
+./scripts/upload-image.sh $ip ./media/profile_pictures/default.png $tok_1
 echo
-./upload-image.sh $ip ./media/profile_pictures/default.png $tok_1
+./scripts/upload-image.sh $ip ./media/profile_pictures/default.png $tok_1
 echo
-./upload-image.sh $ip ./media/profile_pictures/default.png $tok_1
+./scripts/upload-image.sh $ip ./media/profile_pictures/default.png $tok_1
 echo
-./upload-image.sh $ip ./media/profile_pictures/default.png $tok_0
+./scripts/upload-image.sh $ip ./media/profile_pictures/default.png $tok_0
 echo
 sleep 6
-./upload-image.sh $ip ./media/profile_pictures/default.png $tok_1
+./scripts/upload-image.sh $ip ./media/profile_pictures/default.png $tok_1
 echo
-./upload-image.sh $ip ./media/profile_pictures/default.png $tok_1
+./scripts/upload-image.sh $ip ./media/profile_pictures/default.png $tok_1
 echo
-./upload-image.sh $ip ./media/profile_pictures/default.png $tok_1
+./scripts/upload-image.sh $ip ./media/profile_pictures/default.png $tok_1
 echo
-./upload-image.sh $ip ./media/profile_pictures/default.png $tok_1
+./scripts/upload-image.sh $ip ./media/profile_pictures/default.png $tok_1
 echo
-./upload-image.sh $ip ./media/profile_pictures/default.png $tok_1
+./scripts/upload-image.sh $ip ./media/profile_pictures/default.png $tok_1
 echo
-./upload-image.sh $ip ./media/profile_pictures/default.png $tok_1
+./scripts/upload-image.sh $ip ./media/profile_pictures/default.png $tok_1
 echo
-./upload-image.sh $ip ./media/profile_pictures/default.png $tok_1
+./scripts/upload-image.sh $ip ./media/profile_pictures/default.png $tok_1
 echo

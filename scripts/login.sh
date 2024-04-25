17c07c66184b4898087c8fa49a286de8fec9eb1b
malinowy5 <malinowy5@proton.me>
2024-04-25 10:27:37 +0200

    fix scripts

--- scripts/login.sh
+++ scripts/login.sh
@@ -7,5 +7,6 @@ curl -s --location --request POST "$path" \
 --header 'Content-Type: text/plain' \
 --data-raw '{
 	"user_name": "'"$2"'",
-	"passwd": "'"$3"'"
+	"passwd": "'"$3"'",
+	"remember_password": '$4'
 }'
